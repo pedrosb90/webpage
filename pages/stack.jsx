abb6661ae958b0f363a2cf8529b1e657169adf51
drope <pstrauch@gmail.com>
2023-07-04 12:28:39 -0300

    stackpage/addeed container with diplomas

--- pages/stack.jsx
+++ pages/stack.jsx
@@ -1,11 +1,24 @@
 import Layout from "../comps/layout";
 import Navbar from "../comps/navbar";
+import Image from "next/image";
+import styles from "../styles/stack.module.css";
+import dipI from "../public/HenryFull.png";
+import dipII from "../public/HenryTA.jpg";
 
 function Stack() {
   return (
     <Layout>
       <Navbar />
-      <div>stack</div>
+      <div className={styles.container}>
+        <div>
+          <h1 className={styles.title}>Tech Skills</h1>
+          <Image src={dipI} height={200} width={280} display="responsive" />
+        </div>
+        <div>
+          <h1 className={styles.title}>Soft Skills</h1>
+          <Image src={dipII} height={200} width={280} display="responsive" />
+        </div>
+      </div>
     </Layout>
   );
 }
