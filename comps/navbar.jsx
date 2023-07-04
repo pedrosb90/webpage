49a2b8897762f5f077bcf6d29d8aa8abbf2755c8
drope <pstrauch@gmail.com>
2023-07-04 11:41:52 -0300

    navbar/conditional rendering colour

--- comps/navbar.jsx
+++ comps/navbar.jsx
@@ -1,14 +1,22 @@
 import Link from "next/link";
 import styles from "../styles/navbar.module.css";
+import { useRouter } from "next/router";
 
 function Navbar({ children }) {
+  const router = useRouter();
+
+  const isRouteSelected = (route) => {
+    return router.pathname === route
+      ? `${styles.link} ${styles.press} ${styles.selected}`
+      : `${styles.link} ${styles.press}`;
+  };
   return (
     <nav className={styles.container}>
       <div className={styles.titleContainer}>
         <h1 className={styles.mainTitle}> Pedro Strauch</h1>
         <h2 className={styles.title}> Full Stack Developer </h2>
       </div>
-      <Link className={`${styles.link} ${styles.press} `} href="/bio">
+      <Link className={isRouteSelected("/bio")} href="/bio">
         Bio
       </Link>
       <Link className={`${styles.link} ${styles.press} `} href="/stack">
