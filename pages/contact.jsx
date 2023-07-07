ad5d1b439cc3d4d9e8a74876bbe04fadd62689f7
drope <pstrauch@gmail.com>
2023-07-07 17:38:05 -0300

    contactpage/ add navbar + layout

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,5 +1,6 @@
 import Layout from "../comps/layout";
 import React, { useState } from "react";
+import Navbar from "../comps/navbar";
 
 function Contact_form() {
   const [name, setName] = useState("");
@@ -10,35 +11,38 @@ function Contact_form() {
     e.preventDefault();
   };
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="name">Name:</label>
-        <input
-          type="text"
-          id="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="message">Message:</label>
-        <textarea
-          id="message"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-        />
-      </div>
-      <button type="submit">Submit</button>
-    </form>
+    <Layout>
+      <Navbar />{" "}
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="message">Message:</label>
+          <textarea
+            id="message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </Layout>
   );
 }
 
