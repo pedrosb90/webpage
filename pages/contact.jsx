a3ad818ec4493992e1fa74829f26a8b7eaca9c5b
drope <pstrauch@gmail.com>
2023-07-07 17:47:47 -0300

    form/container colour and desing

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -2,6 +2,8 @@ import Layout from "../comps/layout";
 import React, { useState } from "react";
 import Navbar from "../comps/navbar";
 
+import styles from "../styles/form.module.css";
+
 function Contact_form() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
@@ -13,35 +15,39 @@ function Contact_form() {
   return (
     <Layout>
       <Navbar />{" "}
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="name">Name:</label>
-          <input
-            type="text"
-            id="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="message">Message:</label>
-          <textarea
-            id="message"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-          />
-        </div>
-        <button type="submit">Submit</button>
-      </form>
+      <div className={styles.container}>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="name">Name:</label>
+            <input
+              type="text"
+              id="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div>
+            <label className={styles.label} htmlFor="email">
+              Email:
+            </label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="message">Message:</label>
+            <textarea
+              id="message"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            />
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
     </Layout>
   );
 }
