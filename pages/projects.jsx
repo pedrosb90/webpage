7593a01206927e7dacb4c79a5fb8f6a96fb7c25f
drope <pstrauch@gmail.com>
2023-07-10 19:49:40 -0300

    layout/ margins corrected

--- pages/projects.jsx
+++ pages/projects.jsx
@@ -4,7 +4,8 @@ import Navbar from "../comps/navbar";
 function Projects() {
   return (
     <Layout>
-      <Navbar /> <div>Estamos en projectos</div>
+      <Navbar />{" "}
+      <div> no tengo un puto projecto, por eso esta pagina esta vacia</div>
     </Layout>
   );
 }
