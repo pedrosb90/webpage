4068151211a1147139a42d1d0f1a54c68d21fca6
drope <pstrauch@gmail.com>
2023-07-11 21:27:29 -0300

    home/video container sizing + cloudinary config

--- pages/index.js
+++ pages/index.js
@@ -3,23 +3,17 @@ import layout from "../styles/landingItems.module.css";
 import Layout from "../comps/layout";
 import Navbar from "../comps/navbar";
 import MediaLinks from "../comps/mediaLinks";
-// import { CldVideoPlayer } from "next-cloudinary";
+import { CldVideoPlayer } from "next-cloudinary";
+import styles from "../styles/video.module.css";
 
 export default function Home() {
   return (
     <Layout>
-      <div className={layout.background}>
-        <div className={`${layout.container}`}>
-          <Navbar />
-          {/* <CldVideoPlayer
-            id="pedvid"
-            src="https://youtu.be/j5lMLbswsT0"
-            height={400}
-            width={400}
-          /> */}
-          <MediaLinks />
-          <Contact_button />
-        </div>
+      <Navbar />
+      <div className={styles.container}>
+        <CldVideoPlayer id="pedvid" src="https://youtube/j5lMLbswsT0" />
+        <MediaLinks />
+        <Contact_button />
       </div>
     </Layout>
   );
