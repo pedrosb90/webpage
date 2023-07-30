0c4b77923670f00b0b625ef08e5ebe1fbfec8fd9
drope <pstrauch@gmail.com>
2023-07-30 09:00:51 -0300

    loader/ added + initial config

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,44 @@
+import React, { useState, useEffect } from "react";
 import Contact_button from "../comps/contact_button";
-import layout from "../styles/landingItems.module.css";
 import Layout from "../comps/layout";
 import Navbar from "../comps/navbar";
+import Loader from "../comps/loader";
 import MediaLinks from "../comps/mediaLinks";
 import { CldVideoPlayer } from "next-cloudinary";
 import styles from "../styles/video.module.css";
+import stylesL from "../styles/loader.module.css";
 
 export default function Home() {
+  const [pageLoading, setPageLoading] = useState(true);
+
+  useEffect(() => {
+    const loadingTimeout = setTimeout(() => {
+      setPageLoading(false);
+    }, 2000);
+    return () => clearTimeout(loadingTimeout);
+  }, []);
+
   return (
     <Layout>
-      <Navbar />
-      <div className={styles.container}>
-        <CldVideoPlayer
-          id="pedvid"
-          src="https://res.cloudinary.com/ds9ebyi8j/video/upload/v1689356009/Pedro_dev_LQ_hz7zzq.mp4"
-          height={100}
-          width={600}
-        />
-        <MediaLinks />
-        <Contact_button />
-      </div>
+      {pageLoading ? (
+        <div className={stylesL.loaderContainer}>
+          <Loader />
+        </div>
+      ) : (
+        <>
+          <Navbar />
+          <div className={styles.container}>
+            <CldVideoPlayer
+              id="pedvid"
+              src="https://res.cloudinary.com/ds9ebyi8j/video/upload/v1689356009/Pedro_dev_LQ_hz7zzq.mp4"
+              height={100}
+              width={600}
+            />
+            <MediaLinks />
+            <Contact_button />
+          </div>
+        </>
+      )}
     </Layout>
   );
 }
