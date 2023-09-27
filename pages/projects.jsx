e3bf65d8ae0173e451be44dad1c441a2fb20209a
drope <pstrauch@gmail.com>
2023-09-27 20:31:12 -0300

    carrousel/ ok + added project pics

--- pages/projects.jsx
+++ pages/projects.jsx
@@ -3,6 +3,8 @@ import Navbar from "../comps/navbar";
 import Image from "next/legacy/image";
 import styles from "../styles/projects.module.css";
 import youtubeiso from "../public/youtube.png";
+import esu from "../public/esuweb_screen.png";
+import medi from "../public/medicinaweb.png";
 
 function Projects() {
   return (
@@ -13,15 +15,15 @@ function Projects() {
           <h1 className={styles.mainTitle}>Meals To Go</h1>
           <h2 className={styles.subTitle}>Restaurant ordering App</h2>{" "}
           {/* <Image src="/marscolony.png" width={220} height={150} alt="mars" /> */}
-          <p className={styles.body}>React-Native e-Commerce Mobile App</p>
+          <p className={styles.text}>React-Native e-Commerce Mobile App</p>
         </div>
         <div className={styles.card}>
           <h1 className={styles.mainTitle}>Odyssey</h1>
           <h2 className={styles.subTitle}>
             The Exo-Planet Colony Manager
           </h2>{" "}
-          {/* <Image src="/marscolony.png" width={220} height={150} alt="mars" /> */}
-          <p className={styles.body}>
+          <Image src="/marscolony.png" width={220} height={150} alt="mars" />
+          <p className={styles.text}>
             Front-End Web Dashboard emmulating a Space Colony Management Panel.
             Next.js - Mock API - Redux - Figma
           </p>
@@ -31,31 +33,26 @@ function Projects() {
           href="https://youtu.be/emK9kB3gcEU"
           target="_blank"
           rel="noopener noreferrer"
-          className={styles.atag}
+          className={styles.card}
         >
           <h1 className={styles.mainTitle}>Med-Connect</h1>
           <h2 className={styles.subTitle}>Clinic Appointment App</h2>
-          <div className={styles.image}>
-            {/* <Image src={youtubeiso} alt="youtube" width={30} height={18} />{" "} */}
-          </div>
-          <p className={styles.body}>
-            Final group bootcamp project AGILE. Web-App - Next.js - Tailwind,
-            SQL, Redux - Express - Railway - User forms, User authentication,
-            Cloudinary, Payment gateway and Mailing confirmations.
+          <Image src={medi} alt="medicina" width={220} height={150} />{" "}
+          <p className={styles.text}>
+            Web-App - Next.js - Tailwind, SQL, Redux - Express - Railway - User
+            forms, User authentication, Cloudinary, Payment gateway and Mailing.
           </p>
         </a>
         <a
           href="https://eduardostrauch.art/"
           target="_blank"
           rel="noopener noreferrer"
-          className={styles.atag}
+          className={styles.card}
         >
           <h1 className={styles.mainTitle}>Eduardo Strauch Art</h1>
           <h2 className={styles.subTitle}>Shopify Store</h2>
-          {/* <div className={styles.image}>
-            <Image src={youtubeiso} alt="youtube" width={30} height={18} />{" "}
-          </div> */}
-          <p className={styles.body}>
+          <Image src={esu} alt="esu" width={220} height={150} />{" "}
+          <p className={styles.text}>
             Shopify store. Personalized UI - Stock Management - Regular updates
           </p>
         </a>
