884ff371503b0ddf308e0bf057ba869dfbc016be
drope <pstrauch@gmail.com>
2024-07-24 16:43:36 -0300

    video / new update - skijump video

--- pages/index.js
+++ pages/index.js
@@ -36,7 +36,7 @@ export default function Home() {
               src="https://res.cloudinary.com/ds9ebyi8j/video/upload/v1720203899/pedvid.mp4"
             /> */}
             <iframe
-              src="https://res.cloudinary.com/ds9ebyi8j/video/upload/v1720223695/WhatsApp_Video_2024-07-05_at_20.53.47_wnz3sr.mp4"
+              src="https://res.cloudinary.com/ds9ebyi8j/video/upload/v1721850041/WhatsApp_Video_2024-07-24_at_16.35.51_k41d0v.mp4"
               frameborder="0"
               width={400}
               height={300}
