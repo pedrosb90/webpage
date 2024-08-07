a603b100625eecff5f31a3d2bf6548df0789ef7e
drope <pstrauch@gmail.com>
2024-08-07 17:54:17 -0300

    bio/ re structure and content update

--- pages/background.jsx
+++ pages/background.jsx
@@ -19,22 +19,36 @@ function Background() {
       {showDiv ? (
         <div className={stylesII.container}>
           <div>
-            <h2>Digital Creator</h2>I am a Digital Product Designer and
+            <h2>Digital Product</h2>I am a Digital Product Designer and
             Full-Stack Developer with a strong background in management and
             innovative skills. My mission is to create exceptional, next-level
-            web and mobile applications. I adventured into the development
-            world, driven by a desire to bring my ideas to life as soon as they
-            came to mind. I prioritize working with libraries and tools that
-            support agile and smart development, ensuring rapid and effective
-            results. I am dedicated to combining attractive visuals with
-            cutting-edge technology and best practices to develop dynamic and
-            engaging applications.
-            <h2>Marketing</h2>
-            As an objective-focused professional, I excel at understanding and
-            executing mission statements. My passion for extreme sports, cinema,
-            computers, and photography fuels my creativity and inspires me to
-            design outstanding web and mobile applications that boost identity
-            communication.
+            web and mobile applications.
+            <br />
+            <br />I adventured into the development world, driven by a desire to
+            bring my ideas to life as soon as they came to mind. I prioritize
+            working with libraries and tools that support agile and smart
+            development, ensuring rapid and effective results. I am dedicated to
+            combining attractive visuals with cutting-edge technology and best
+            practices to develop dynamic and engaging applications.
+            <h2>Video Content</h2>
+            In a world where visuals speak louder than words, my video creation
+            service is dedicated to producing high-quality footage and
+            illustrations that captivate and inspire. I am passionate about
+            standing out from the crowd, bringing a fresh perspective to every
+            project with a focus on relevance, originality, and impact.
+            <br />
+            <br />
+            Every frame tells a story, and I strive to ensure that each story is
+            not only visually stunning but also deeply meaningful. With an eye
+            for detail and a commitment to excellence, I craft videos that
+            resonate with your audience and leave a lasting impression.
+            <br />
+            <br />
+            Whether you're looking to promote a brand, tell a personal story, or
+            convey a powerful message, my video creation service will elevate
+            your vision to new heights. Let’s create something extraordinary
+            together that truly stands out and speaks to the world.
+            <br />
           </div>
         </div>
       ) : (
