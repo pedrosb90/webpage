e921686515e88f092ae8a75b05604b08ccdd952c
drope <pstrauch@gmail.com>
2024-08-07 10:35:29 -0300

    loader fix/ moved it to ap environment

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,31 @@
-import React from "react";
-import App from "next/app";
+import React, { useState, useEffect } from "react";
 import "../styles/fonts.css";
 import Layout from "../comps/layout";
+import Loader from "../comps/loader";
+import stylesL from "../styles/loader.module.css";
 
-class MyApp extends App {
-  render() {
-    const { Component, pageProps } = this.props;
-    return (
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    );
-  }
+function MyApp({ Component, pageProps }) {
+  const [pageLoading, setPageLoading] = useState(true);
+
+  useEffect(() => {
+    const loadingTimeout = setTimeout(() => {
+      setPageLoading(false);
+    }, 1500);
+
+    return () => clearTimeout(loadingTimeout);
+  }, []);
+
+  return pageLoading ? (
+    <Layout>
+      <div className={stylesL.loaderContainer}>
+        <Loader />
+      </div>
+    </Layout>
+  ) : (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
 export default MyApp;
