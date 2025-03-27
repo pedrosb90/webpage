b027053e58514482143cf64849e533cc57e11dd2
drope <pstrauch@gmail.com>
2025-03-27 17:13:14 -0300

    adjustment

--- comps/mediaLinks.jsx
+++ comps/mediaLinks.jsx
@@ -1,53 +1,16 @@
 import Image from "next/legacy/image";
-import git from "../public/github logoiso.png";
-import linke from "../public/linkedin logo.png";
-import insta from "../public/logo-instagram.png";
+import Reactlogo from "../public/Reactlogo.png";
+import Shopifylogo from "../public/shopilogo.png";
+import Webflowlogo from "../public/webflow.png";
 import layouts from "../styles/mediaLinks.module.css";
 
 function MediaLinks() {
   return (
-    <div className={layouts.container}>
-      <a
-        target="_blank"
-        rel="noopener noreferrer"
-        href="https://github.com/pedrosb90/"
-      >
-        <Image
-          className={`${layouts.press} ${layouts.style}`}
-          alt="git"
-          src={git}
-          height={80}
-          width={175}
-        />
-      </a>
-      <a
-        href="https://www.linkedin.com/in/pedrostrauchbraga/"
-        target="_blank"
-        rel="noopener noreferrer"
-        className={layouts.style}
-      >
-        <Image
-          className={`${layouts.press} ${layouts.style}`}
-          alt="linke"
-          src={linke}
-          height={80}
-          width={90}
-        />
-      </a>
-      <a
-        href="https://www.instagram.com/pedrstrauch/"
-        target="_blank"
-        rel="noopener noreferrer"
-        className={layouts.style}
-      >
-        <Image
-          className={`${layouts.press} ${layouts.styleInsta}`}
-          alt="insta"
-          src={insta}
-          height={80}
-          width={90}
-        />
-      </a>
+    <div className={layouts.style}>
+      {" "}
+      <Image alt="Reactlogo" src={Reactlogo} height={50} width={170} />
+      <Image alt="Shopifylogo" src={Shopifylogo} height={50} width={200} />
+      <Image alt="Webflowlogo" src={Webflowlogo} height={50} width={200} />
     </div>
   );
 }
