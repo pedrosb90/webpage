cc911520bf734e8793c35362bdb3cb691f8ec8b4
drope <pstrauch@gmail.com>
2025-03-27 16:06:32 -0300

    home/new cards/ mobiel ok/

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ import StructureLay from "../comps/structureLay";
 import Navbar from "../comps/navbar";
 import MediaLinks from "../comps/mediaLinks";
 import styles from "../styles/video.module.css";
-import stylesii from "../styles/projects.module.css";
 import stylesiii from "../styles/infoservices.module.css";
 
 export default function Home({}) {
@@ -13,7 +12,7 @@ export default function Home({}) {
         <Navbar />
 
         <div className={styles.container}>
-          <div className={stylesii.carousel}>
+          <div className={stylesiii.carousel}>
             <a className={stylesiii.card}>Hola</a>
             <a className={stylesiii.card}>Que</a>
           </div>
